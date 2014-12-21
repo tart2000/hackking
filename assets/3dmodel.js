d0fb4f7132d01b1f31dfa82e07f5b673df87df2e
Emmanuel Durand <emmanueldurand@gmail.com>
2014-12-20 21:56:56 -0500

    Moved the gl renderer into its canva

--- assets/3dmodel.js
+++ assets/3dmodel.js
@@ -1,15 +1,28 @@
 var _scene, _camera, _renderer;
-var _objLoader;
+var _model;
 
 /*************/
 var init = function() {
   _scene = new THREE.Scene();
   _camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+  _camera.position.set(0, 0, 10);
+  _camera.lookAt(new THREE.Vector3(0.0, 0.0, 0.0));
+  _scene.add(_camera);
   
   _renderer = new THREE.WebGLRenderer();
   _renderer.setSize(512, 512);
   _renderer.setClearColor(0xaaaaaa, 0xff);
-  document.body.appendChild(_renderer.domElement);
+  var canvas = document.getElementById("3dcanvas");
+  canvas.appendChild(_renderer.domElement);
+
+  var jsLoader = new THREE.JSONLoader(true);
+  jsLoader.load('assets/models/angle.json', function(geometry, material) {
+    _model = new THREE.Mesh(geometry, material);
+    _scene.add(_model);
+  });
+
+  var ambientLight = new THREE.AmbientLight(0xffffff);
+  _scene.add(ambientLight);
 }
 
 /*************/
@@ -17,6 +30,7 @@ var render = function(timestamp) {
   requestAnimationFrame(render);
 
   _renderer.clear();
+  _renderer.render(_scene, _camera);
 }
 
 init();
